4e56f14623a52d7ab0a95031a0878ea67f4ebd56
haripriya <rm.haripriya@gmail.com>
2023-11-08 19:26:39 +0530

    Cleanup

--- src/main/java/com/techatpark/workout/controller/ProfileAPIController.java
+++ src/main/java/com/techatpark/workout/controller/ProfileAPIController.java
@@ -1,6 +1,5 @@
 package com.techatpark.workout.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gurukulams.core.payload.Learner;
 import com.gurukulams.core.service.LearnerService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,11 +10,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import java.security.Principal;
 import java.sql.SQLException;
@@ -24,9 +20,9 @@ import java.sql.SQLException;
  * The type Learner api controller.
  */
 @RestController
-@RequestMapping("/api/learner")
-@Tag(name = "Learners", description = "Resources to manage Learner")
-class LearnerAPIController {
+@RequestMapping("/api/profiles")
+@Tag(name = "Profiles", description = "Resources to manage Profiles")
+class ProfileAPIController {
 
     /**
      * declare a learner service.
@@ -36,7 +32,7 @@ class LearnerAPIController {
     /**
      * @param alearnerService a learner service
      */
-    LearnerAPIController(final LearnerService alearnerService) {
+    ProfileAPIController(final LearnerService alearnerService) {
         this.learnerService = alearnerService;
     }
 
@@ -56,29 +52,5 @@ class LearnerAPIController {
         return ResponseEntity.of(learnerService.read(
                 id));
     }
-
-    @Operation(summary = "Updates the learner by given id",
-            description = "Can be called only by users "
-                    + "with 'auth management' rights.",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponses(value = {@ApiResponse(responseCode = "200",
-            description = "learner updated successfully"),
-            @ApiResponse(responseCode = "400",
-                    description = "learner is invalid"),
-            @ApiResponse(responseCode = "401",
-                    description = "invalid credentials"),
-            @ApiResponse(responseCode = "404",
-                    description = "learner not found")})
-    @PutMapping(value = "/{id}", produces = "application/json", consumes =
-            "application/json")
-    public final ResponseEntity<Learner> update(final @PathVariable String id,
-                                          final Principal principal,
-                                          final @RequestBody Learner learner)
-            throws JsonProcessingException, SQLException {
-        final Learner updatedLearner = learnerService.update(id,
-                learner);
-        return ResponseEntity.ok(updatedLearner);
-    }
-
 
 }
