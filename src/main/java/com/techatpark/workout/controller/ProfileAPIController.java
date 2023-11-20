efacecb224010e4c1f84fb54899934c75d927480
haripriya <rm.haripriya@gmail.com>
2023-11-20 13:53:40 +0530

    Login Issues 2

--- src/main/java/com/techatpark/workout/controller/ProfileAPIController.java
+++ src/main/java/com/techatpark/workout/controller/ProfileAPIController.java
@@ -1,6 +1,8 @@
 package com.techatpark.workout.controller;
 
+import com.gurukulams.core.model.Org;
 import com.gurukulams.core.payload.Profile;
+import com.gurukulams.core.service.OrgService;
 import com.gurukulams.core.service.ProfileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,11 +12,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * The type Profile api controller.
@@ -29,11 +34,20 @@ class ProfileAPIController {
      */
     private final ProfileService profileService;
 
+
     /**
-     * @param profileService1 a learner service
+     * declare a org Service.
      */
-    ProfileAPIController(final ProfileService profileService1) {
-        this.profileService = profileService1;
+    private final OrgService orgService;
+
+    /**
+     * @param theProfileService a learner service
+     * @param theOrgService a Org service
+     */
+    ProfileAPIController(final ProfileService theProfileService,
+                         final OrgService theOrgService) {
+        this.profileService = theProfileService;
+        this.orgService = theOrgService;
     }
 
 
@@ -51,6 +65,27 @@ class ProfileAPIController {
             throws SQLException {
         return ResponseEntity.of(profileService.read(
                 id));
+    }
+
+    @Operation(summary = "Get the Profile with given id",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "getting learner successfully"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "learner not found")})
+    @GetMapping(value = "/{id}/orgs", produces = "application/json")
+    public final ResponseEntity<List<Org>> getOrgs(final Principal principal,
+                                   final @PathVariable String id,
+                                   @RequestHeader(name = "Accept-Language",
+                                       required = false) final Locale locale)
+            throws SQLException {
+        final List<Org> orgs = orgService.getOrganizationsOf(
+                id,
+                locale);
+        return orgs.isEmpty() ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(orgs);
     }
 
 }
