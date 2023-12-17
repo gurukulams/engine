c2ae1389ca440552a3bbb5867327fa912653a72a
sathishk <sathishkumar.thiyagarajan@gmail.com>
2023-12-17 19:36:55 +0530

    Follow User

--- src/main/java/com/techatpark/workout/controller/ProfileAPIController.java
+++ src/main/java/com/techatpark/workout/controller/ProfileAPIController.java
@@ -12,8 +12,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -88,4 +90,71 @@ class ProfileAPIController {
                 : ResponseEntity.ok(orgs);
     }
 
+    @Operation(summary = "Get the buddies with given id",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "getting learner successfully"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "learner not found")})
+    @GetMapping(value = "/{id}/buddies", produces = "application/json")
+    public final ResponseEntity<List<Profile>> getBuddies(
+                                final Principal principal,
+                                   final @PathVariable String id)
+            throws SQLException {
+        final List<Profile> buddies = profileService.getBuddies(
+                id);
+        return buddies.isEmpty() ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(buddies);
+    }
+    /**
+     * Register a Event.
+     *
+     * @param id
+     * @param principal
+     * @return event
+     */
+    @Operation(summary = "Registers the event by given id",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "event registered successfully"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "event not found")})
+    @RequestMapping(method = RequestMethod.HEAD, value = "/{id}")
+    public final ResponseEntity<Void> isRegistered(@PathVariable final
+                                                   String id,
+                                                   final Principal principal)
+            throws SQLException {
+        return profileService.isRegistered(principal.getName(),
+                id) ? ResponseEntity.ok().build()
+                : ResponseEntity.notFound().build();
+    }
+
+    /**
+     * Register a Event.
+     *
+     * @param id
+     * @param principal
+     * @return event
+     */
+    @Operation(summary = "Registers the event by given id",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "event registered successfully"),
+            @ApiResponse(responseCode = "401",
+                    description = "invalid credentials"),
+            @ApiResponse(responseCode = "404",
+                    description = "event not found")})
+    @PostMapping("/{id}")
+    public final ResponseEntity<Void> register(@PathVariable final
+                                               String id,
+                                               final Principal principal)
+            throws SQLException {
+        return profileService.register(principal.getName(),
+                id) ? ResponseEntity.ok().build()
+                : ResponseEntity.notFound().build();
+    }
 }
